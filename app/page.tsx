dd10c19c39121cdd5f1e3adad99ca8fa68c44ef2
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2025-02-07 16:02:43 +0530

    Enhance mobile experience by adding viewport settings and CSS adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
       <div className="flex justify-center items-center h-screen">
-        <h1>Hi, I am Muhammed  Shibil M</h1>
+        <h1 className="bg-red-500 md:bg-green-500 lg:bg-violet-500 xl:bg-gray-500 ">Hi, M</h1>
+
       </div>
   );
 }
